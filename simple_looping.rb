41930babb1775307f4b3640298708029dec8f46e
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-18 03:32:49 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,7 +1,13 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-
+  counter = 1 
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  loop do 
+    puts phrase 
+    break if counter == number_of_times
+    counter += 1
+end
 end
 
 def times_iterator(number_of_times)
